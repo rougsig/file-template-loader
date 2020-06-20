1aa8d4e10eef259b75658129cf672d2a1a27f485
rougsig <rougsig@gmail.com>
2020-06-21 01:51:13 +0200

    fix module finding

--- ftl/src/main/kotlin/com/github/rougsig/ftl/extenstion/VirtualFileExtenstions.kt
+++ ftl/src/main/kotlin/com/github/rougsig/ftl/extenstion/VirtualFileExtenstions.kt
@@ -6,5 +6,5 @@ import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.kotlin.idea.util.projectStructure.allModules
 
 internal fun VirtualFile.findModule(project: Project): Module? {
-  return project.allModules().find { it.moduleFilePath.contains(this.path) }
+  return project.allModules().find { this.path.contains(it.moduleFile?.parent?.path ?: "") }
 }
