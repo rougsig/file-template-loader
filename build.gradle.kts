2d10d9ed594d351753782fa1e320074badefd4bc
LionZXY <nikita@kulikof.ru>
2021-09-23 19:38:53 +0300

    Update to newest IDE

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    // Java support
+    id("java")
+    // Kotlin support
+    id("org.jetbrains.kotlin.jvm") version "1.5.30"
+    // Gradle IntelliJ Plugin
+    id("org.jetbrains.intellij") version "1.1.6"
+}
+
+group = properties("pluginGroup")
+version = properties("pluginVersion")
+
+intellij {
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType"))
+    downloadSources.set(properties("platformDownloadSources").toBoolean())
+    updateSinceUntilBuild.set(true)
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    properties("javaVersion").let {
+        withType<JavaCompile> {
+            sourceCompatibility = it
+            targetCompatibility = it
+        }
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = it
+        }
+    }
+
+    wrapper {
+        gradleVersion = properties("gradleVersion")
+    }
+
+    patchPluginXml {
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+        pluginDescription.set("""
+            Improved template engine for IntelliJ IDEA to generate templates, their groups and modules.
+            Supports the creation of multiple files organized in subdirectories, and insert code into existing files.
+            Allows you to use the parameters you want to enter when you create a template.
+        """.trimIndent())
+    }
+}
